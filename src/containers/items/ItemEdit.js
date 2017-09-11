74c4ec869b54cda75416d850f989015d6a57b33c
Jeremy Hoffing <jcohenhoffing@gmail.com>
2017-09-10 20:46:46 -0700

    Update item edit with new style

--- src/containers/items/ItemEdit.js
+++ src/containers/items/ItemEdit.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Form, Input, Textarea } from 'formsy-react-components'
-import { selectItem } from '../../actions/actions_items'
-import { createItem } from '../../actions/actions_items'
-import { item } from '../../models/selectors'
 
-import '../../style/item.css'
+import { createItem, selectItem, updateItem } from '../../actions/actions_items'
+import { item } from '../../models/selectors'
+import '../../style/new-listing.css'
 
 class ItemEdit extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      category: '',
+      name: '',
+      short_description: '',
+      description: '',
+      picture: '',
+      reference: '',
+      price: '',
+      discount: '0.0',
+      shipping_fee: '0.0',
+      shipping_deadline: 7
+    }
   }
 
   handleChange(name, value) {
@@ -17,7 +29,7 @@ class ItemEdit extends Component {
   }
 
   submit(model) {
-    this.props.createItem(this.props)
+    this.props.updateItem(this.state)
   }
 
   componentWillMount() {
@@ -35,103 +47,91 @@ class ItemEdit extends Component {
   renderForm() {
     return (
       <Form layout='vertical' className='item-form' onValidSubmit={this.submit.bind(this)}>
-        <div className='pure-g'>
-          <div className='pure-u-1 pure-u-md-1-2'>
-            <Input
-              label='Name'
-              value={this.props.item.name}
-              name='name'
-              type='text'
-              onChange={this.handleChange.bind(this)}
-              required />
-          </div>
-          <div className='pure-u-1 pure-u-md-1-2'>
-            <Input
-              label='Reference ID'
-              value={this.props.item.reference}
-              name='reference'
-              type='text'
-              onChange={this.handleChange.bind(this)}
-               />
-          </div>
-          <div className='pure-u-1 pure-u-md-1-2'>
-            <Input
-              label='Price'
-              value={this.props.item.price}
-              name='price'
-              type='text'
-              onChange={this.handleChange.bind(this)}
-              required />
-          </div>
-          <div className='pure-u-1 pure-u-md-1-2'>
-            <Input
-              label='Discount (%)'
-              value={this.props.item.discount}
-              name='discount'
-              type='text'
-              onChange={this.handleChange.bind(this)}
-               />
-          </div>
-          <div className='pure-u-1 pure-u-md-1-2'>
-            <Input
-              label='Shipping Fee'
-              value={this.props.item.shipping_fee}
-              name='shipping_fee'
-              type='text'
-              onChange={this.handleChange.bind(this)}
-               />
-          </div>
-          <div className='pure-u-1 pure-u-md-1-2'>
-            <Input
-              label='Picture'
-              value={this.props.item.picture}
-              name='picture'
-              type='text'
-              onChange={this.handleChange.bind(this)}
-              required />
-          </div>
-          <div className='pure-u-1 pure-u-md-1-2'>
-            <Input
-              label='Shipping Deadline'
-              value={this.props.item.shipping_deadline}
-              name='shipping_deadline'
-              type='text'
-              onChange={this.handleChange.bind(this)}
-              required />
-          </div>
-          <div className='pure-u-1'>
-            <Textarea
-              label="Short Description"
-              value={this.props.item.short_description}
-              name="short_description"
-              type="text"
-              onChange={this.handleChange.bind(this)}
-              rows={3}
-             />
-          </div>
-          <div className='pure-u-1'>
-            <Textarea
-              label="Long Description"
-              value={this.props.item.description}
-              name="description"
-              type="text"
-              onChange={this.handleChange.bind(this)}
-              rows={10}
-            />
-          </div>
+        <Input
+          label='Name'
+          value={this.props.item.name}
+          name='name'
+          type='text'
+          onChange={this.handleChange.bind(this)}
+          required />
+        <Input
+          label='Reference ID'
+          value={this.props.item.reference}
+          name='reference'
+          type='text'
+          onChange={this.handleChange.bind(this)}
+           />
+        <Input
+          label='Price'
+          value={this.props.item.price}
+          name='price'
+          type='text'
+          onChange={this.handleChange.bind(this)}
+          required />
+        <Input
+          label='Discount (%)'
+          value={this.props.item.discount}
+          name='discount'
+          type='text'
+          onChange={this.handleChange.bind(this)}
+           />
+        <Input
+          label='Shipping Fee'
+          value={this.props.item.shipping_fee}
+          name='shipping_fee'
+          type='text'
+          onChange={this.handleChange.bind(this)}
+           />
+        <Input
+          label='Picture'
+          value={this.props.item.picture}
+          name='picture'
+          type='text'
+          onChange={this.handleChange.bind(this)}
+          required />
+        <Input
+          label='Shipping Deadline'
+          value={this.props.item.shipping_deadline}
+          name='shipping_deadline'
+          type='text'
+          onChange={this.handleChange.bind(this)}
+          required />
+        <Textarea
+          label="Short Description"
+          value={this.props.item.short_description}
+          name="short_description"
+          type="text"
+          onChange={this.handleChange.bind(this)}
+          rows={3}
+         />
+        <Textarea
+          label="Long Description"
+          value={this.props.item.description}
+          name="description"
+          type="text"
+          onChange={this.handleChange.bind(this)}
+          rows={10}
+        />
+        <div className="form-action">
+          <button type="submit" className="btn-shadow btn-shadow-dark">Create Listing</button>
         </div>
-        <button className="pure-button pure-button-primary"
-                type='submit'>Create Item</button>
       </Form>
     )
   }
 
   render() {
     return (
-      <div className='item'>
-        <h3 className='title'>EDIT ITEM {this.props.item.id}</h3>
-        <div className='title-divider'></div>
-        { this.renderForm() }
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="ecommerce-form">
+              <h1>
+                Create a listing
+              </h1>
+              { this.renderForm() }
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
@@ -141,5 +141,4 @@ function mapStateToProps(state) {
   return { item: item(state), provider: state.provider }
 }
 
-
-export default connect(mapStateToProps, { selectItem, createItem })(ItemEdit)
+export default connect(mapStateToProps, { selectItem })(ItemEdit)
