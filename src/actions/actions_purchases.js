2958453e8f0da116cab7a5bb6bece88203f4941c
Fabrice Cheng <fabrice@collabcreators.com>
2017-09-18 21:04:10 -0700

    remmove ItemShipping components

--- src/actions/actions_purchases.js
+++ src/actions/actions_purchases.js
@@ -56,9 +56,9 @@ export function selectPurchase(provider, purchaseId) {
     axios.get(`${PURCHASES_URL}/${purchaseId}`, headers)
          .then((request) => {
             const normalizeRequest = normalize(request.data, purchaseNormalizr)
-            dispatch({ type: CREATE_PURCHASES, payload: normalizeRequest.entities.purchases })
             dispatch({ type: CREATE_ITEMS, payload: normalizeRequest.entities.items })
             dispatch({ type: CREATE_USERS, payload: normalizeRequest.entities.users })
+            dispatch({ type: CREATE_PURCHASES, payload: normalizeRequest.entities.purchases })
             dispatch({ type: SELECT_PURCHASE, payload: request.data.id })
             dispatch({ type: SELECT_ITEM, payload: request.data.item.id} )
             dispatch({ type: SELECT_USER, payload: request.data.buyer.wallet })
