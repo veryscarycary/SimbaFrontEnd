ce1308d777d92e5ca265a2b063875abe47786b57
Jeremy Hoffing <jcohenhoffing@gmail.com>
2017-09-22 09:21:39 -0700

    set state on first pageload

--- src/containers/items/ItemEdit.js
+++ src/containers/items/ItemEdit.js
@@ -10,37 +10,64 @@ class ItemEdit extends Component {
     super(props)
 
     this.state = {
-      category: '',
       name: '',
-      short_description: '',
-      description: '',
-      picture: '',
-      reference: '',
+      category: '',
       price: '',
-      discount: '0.0',
-      shipping_fee: '0.0',
-      shipping_deadline: 7
+      reference: '',
+      discount: '',
+      shipping_fee: '',
+      picture: '',
+      shipping_deadline: '',
+      short_description: '',
+      description: ''
     }
   }
+
+
 
   handleChange(name, value) {
     this.setState({[name]: value})
   }
 
   submit(model) {
-    this.props.updateItem(model, this.props.item.id)
-    this.props.history.push('/my_items')
+    this.props.updateItem(model, this.props.item.id).then((response) => {
+      this.props.history.push('/my_items')
+    })
   }
 
   componentWillMount() {
     if (this.props.provider.eth) {
       this.props.selectItem(this.props.provider, this.props.match.params.item_id)
+      this.setState({
+        name: this.props.item.name,
+        reference: this.props.item.reference,
+        price: this.props.item.price,
+        discount: this.props.item.discount,
+        shipping_fee: this.props.item.shipping_fee,
+        picture: this.props.item.picture,
+        shipping_deadline: this.props.item.shipping_deadline,
+        short_description: this.props.item.short_description,
+        description: this.props.item.description
+      })
     }
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.provider.eth && !this.props.provider.eth) {
       this.props.selectItem(nextProps.provider, this.props.match.params.item_id)
+    }
+    if (nextProps.item) {
+      this.setState({
+        name: nextProps.item.name,
+        reference: nextProps.item.reference,
+        price: nextProps.item.price,
+        discount: nextProps.item.discount,
+        shipping_fee: nextProps.item.shipping_fee,
+        picture: nextProps.item.picture,
+        shipping_deadline: nextProps.item.shipping_deadline,
+        short_description: nextProps.item.short_description,
+        description: nextProps.item.description
+      })
     }
   }
 
@@ -49,56 +76,56 @@ class ItemEdit extends Component {
       <Form layout='vertical' className='item-form' onValidSubmit={this.submit.bind(this)}>
         <Input
           label='Name'
-          value={this.props.item.name}
+          value={this.state.name}
           name='name'
           type='text'
           onChange={this.handleChange.bind(this)}
           required />
         <Input
           label='Reference ID'
-          value={this.props.item.reference}
+          value={this.state.reference}
           name='reference'
           type='text'
           onChange={this.handleChange.bind(this)}
            />
         <Input
           label='Price'
-          value={this.props.item.price}
+          value={this.state.price}
           name='price'
           type='text'
           onChange={this.handleChange.bind(this)}
           required />
         <Input
           label='Discount (%)'
-          value={this.props.item.discount}
+          value={this.state.discount}
           name='discount'
           type='text'
           onChange={this.handleChange.bind(this)}
            />
         <Input
           label='Shipping Fee'
-          value={this.props.item.shipping_fee}
+          value={this.state.shipping_fee}
           name='shipping_fee'
           type='text'
           onChange={this.handleChange.bind(this)}
            />
         <Input
           label='Picture'
-          value={this.props.item.picture}
+          value={this.state.picture}
           name='picture'
           type='text'
           onChange={this.handleChange.bind(this)}
           required />
         <Input
           label='Shipping Deadline'
-          value={this.props.item.shipping_deadline}
+          value={this.state.shipping_deadline}
           name='shipping_deadline'
           type='text'
           onChange={this.handleChange.bind(this)}
           required />
         <Textarea
           label="Short Description"
-          value={this.props.item.short_description}
+          value={this.state.short_description}
           name="short_description"
           type="text"
           onChange={this.handleChange.bind(this)}
@@ -106,7 +133,7 @@ class ItemEdit extends Component {
          />
         <Textarea
           label="Long Description"
-          value={this.props.item.description}
+          value={this.state.description}
           name="description"
           type="text"
           onChange={this.handleChange.bind(this)}
