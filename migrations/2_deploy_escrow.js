7c467a252b32294d82b26af77e0d238c4240040c
Fabrice Cheng <fabrice@collabcreators.com>
2017-09-30 11:10:06 -0700

    remove unnecessary arguments

--- migrations/2_deploy_escrow.js
+++ migrations/2_deploy_escrow.js
@@ -1,4 +1,4 @@
 var Escrow = artifacts.require("./Escrow.sol");
 module.exports = function(deployer) {
-  deployer.deploy(Escrow, "0x08b41D8A406F941126fc1D15eCe163fbFA113d54", "0x0Aa7fa099de11980486A11Bcf2865f48f6a90128");
+  deployer.deploy(Escrow);
 };
