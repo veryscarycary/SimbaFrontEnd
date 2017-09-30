faf1bdd346448a48d41838ab39b594519ea93a8e
Fabrice Cheng <fabrice@collabcreators.com>
2017-09-30 11:11:06 -0700

    add "from accounts" parameters to specify from whcih account to deploy

--- truffle.js
+++ truffle.js
@@ -9,6 +9,7 @@ module.exports = {
       network_id: '*', // Match any network id,
       gas: 3000000, // new! Default gas limit is 3141592
       gasPrice: 100000000000, // new! Default gas price is 100 Shannon
+      from: '0x3d208fabaf7e919985d5d2f068f28d6a9022e8d5',
     }
   }
 }
