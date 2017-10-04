236fb96ef3b092675a3f5e02470c2e6c82e6800d
Rudth-Mael <rm@galite.net>
2017-10-04 07:53:50 +0200

    Put back watcher events

--- src/actions/actions_event_watcher.js
+++ src/actions/actions_event_watcher.js
@@ -7,6 +7,127 @@ import { createLogActivity } from './actions_activities'
 import { purchaseState, activityCategories } from '../containers/shared/PurchaseState'
 
 // Watch block chain event
+// Buyer purchases an Item
+export function watchPurchaseEvent(provider) {
+  const escrow = contract(escrowJSON)
+  escrow.setProvider(provider.eth.currentProvider)
+
+  return dispatch => {
+    escrow.deployed().then(instance => {
+    instance.ItemPurchased().watch(function(error, result) {
+      if (!error) {
+        const newLog = `[${result.args.buyer}] purchased ${Eth.toUtf8(result.args.purchaseId)} for ${Eth.fromWei(result.args.amount, 'ether')} ETH`
+        console.log('[Event - ItemPurchased] : ', newLog)
+        dispatch(createLogActivity(activityCategories.PURCHASE,
+                                   Eth.toUtf8(result.args.purchaseId),
+                                   Eth.toUtf8(result.args.itemId),
+                                   Eth.fromWei(result.args.amount, 'ether'),
+                                   result.args.buyer,
+                                   result.args.seller)
+        )
+        dispatch({type: CREATE_PURCHASE, payload: {id: Eth.toUtf8(result.args.purchaseId), purchaseState: purchaseState.PURCHASED}})
+      } else {
+        console.log(error)
+        dispatch({type: CREATE_PURCHASE, payload: {id: Eth.toUtf8(result.args.purchaseId), purchaseState: purchaseState.ERROR}})
+      }
+    })
+  })
+  }
+}
+
+// Watch block chain event
+// User shipped item and send code (i.e : tracking number)
+export function watchShippingEvent(provider) {
+  const escrow = contract(escrowJSON)
+  escrow.setProvider(provider.eth.currentProvider)
+
+  return dispatch => {
+    escrow.deployed().then(instance => {
+      instance.ItemShipped().watch(function(error, result) {
+        if (!error) {
+          const newLog = `[${result.args.seller}] shipped ${Eth.toUtf8(result.args.itemId)} - Tracking Number : ${Eth.toUtf8(result.args.code)}`
+          console.log('[Event - ItemShipped] : ', newLog)
+          dispatch(createLogActivity(activityCategories.SHIP_ITEM,
+                                     Eth.toUtf8(result.args.purchaseId),
+                                     Eth.toUtf8(result.args.itemId),
+                                     Eth.toAscii(result.args.code),
+                                     result.args.buyer,
+                                     result.args.seller)
+          )
+          dispatch({type: CREATE_PURCHASE, payload: {id: Eth.toUtf8(result.args.purchaseId), purchaseState: purchaseState.SHIPPED}})
+        } else {
+          dispatch({type: CREATE_PURCHASE, payload: {id: Eth.toUtf8(result.args.purchaseId), purchaseState: purchaseState.ERROR}})
+        }
+      })
+    })
+  }
+}
+
+// Watch block chain event
+// User confirm the purchase closing the transaction and paying the seller
+export function watchPurchaseCompleteEvent(provider) {
+  const escrow = contract(escrowJSON)
+  escrow.setProvider(provider.eth.currentProvider)
+
+  return dispatch => {
+    escrow.deployed().then(instance => {
+      instance.PurchaseCompleted().watch(function(error, result) {
+        if (!error) {
+          const newLog = `[${result.args.buyer}] confirms receiving ${Eth.toUtf8(result.args.itemId)}. The transaction is complete.`
+          console.log('[Event - PurchaseComplete] : ', newLog)
+          dispatch(createLogActivity(activityCategories.CONFIRM_PURCHASE,
+                                     Eth.toUtf8(result.args.purchaseId),
+                                     Eth.toUtf8(result.args.itemId),
+                                     '',
+                                     result.args.buyer,
+                                     result.args.seller)
+          )
+          dispatch({type: CREATE_PURCHASE, payload: {id: Eth.toUtf8(result.args.purchaseId), purchaseState: purchaseState.COMPLETED}})
+        } else {
+          console.log(error)
+          dispatch({type: CREATE_PURCHASE, payload: {id: Eth.toUtf8(result.args.purchaseId), purchaseState: purchaseState.ERROR}})
+        }
+      })
+    })
+  }
+}
+
+// Watch block chain event
+// When a buyer cancel a purchases before the item was shipped
+export function watchCancelPurchaseEvent(provider) {
+  const escrow = contract(escrowJSON)
+  escrow.setProvider(provider.eth.currentProvider)
+
+  return dispatch => {
+    escrow.deployed().then(instance => {
+      instance.PurchaseCancelled().watch(function(error, result) {
+        if (!error) {
+          const newLog = `[${result.args.sender}] cancels order ${Eth.toUtf8(result.args.itemId)}.`
+          console.log('[Event - CancelPurchase] : ', newLog)
+          var activityCategory = activityCategories.CANCEL_PURCHASE
+          var cancelPurchaseState = purchaseState.BUYER_CANCELLED
+          if (result.args.sender === result.args.seller) {
+            activityCategory = activityCategories.CANCEL_SALES
+            cancelPurchaseState = purchaseState.SELLER_CANCELLED
+          }
+          dispatch(createLogActivity(activityCategory,
+                                     Eth.toUtf8(result.args.purchaseId),
+                                     Eth.toUtf8(result.args.itemId),
+                                     '',
+                                     result.args.buyer,
+                                     result.args.seller)
+          )
+          dispatch({type: CREATE_PURCHASE, payload: {id: Eth.toUtf8(result.args.purchaseId), purchaseState: cancelPurchaseState}})
+        } else {
+          console.log(error)
+          dispatch({type: CREATE_PURCHASE, payload: {id: Eth.toUtf8(result.args.purchaseId), purchaseState: purchaseState.ERROR}})
+        }
+      })
+    })
+  }
+}
+
+// Watch block chain event
 // Purchases is automatically cancelled if Item hasn't been shipped before the Shipping Deadline
 export function watchShippingTimeoutEvent(provider) {
   const escrow = contract(escrowJSON)
