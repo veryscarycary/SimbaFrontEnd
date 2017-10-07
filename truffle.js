8242c62b8003474e67e28247e07789c697adad28
Fabrice Cheng <fabrice@collabcreators.com>
2017-10-06 18:05:38 -0700

    change gas price

--- truffle.js
+++ truffle.js
@@ -7,7 +7,7 @@ module.exports = {
       host: 'localhost',
       port: 8545,
       network_id: '*', // Match any network id,
-      gas: 3000000, // new! Default gas limit is 3141592
+      gas: 4700000, // new! Default gas limit is 3141592
       gasPrice: 100000000000, // new! Default gas price is 100 Shannon
       from: '0x3d208fabaf7e919985d5d2f068f28d6a9022e8d5',
     }
