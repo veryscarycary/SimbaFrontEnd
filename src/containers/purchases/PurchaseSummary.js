9a19d195464dc32a4b288ee2605717a381778def
Fabrice Cheng <fabrice@collabcreators.com>
2017-11-30 17:48:09 -0800

    convert price to ETH

--- src/containers/purchases/PurchaseSummary.js
+++ src/containers/purchases/PurchaseSummary.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
+import ETHPriceConverter from '../shared/ETHPriceConverter'
+
 class PurchaseSummary extends Component {
   renderDiscount() {
     if (!this.props.item.discount || this.props.item.discount === 0) {
@@ -10,7 +12,7 @@ class PurchaseSummary extends Component {
     return (
       <div className="detail clearfix">
         <p>Discount</p>
-        <span>- { discountedPrice } ETH</span>
+        <span>- ${ discountedPrice }</span>
       </div>
     )
   }
@@ -52,7 +54,7 @@ class PurchaseSummary extends Component {
             </div>
             <div className="total-price clearfix">
               <p>Total payment</p>
-              <span>${ this.props.finalPrice }</span>
+              <span>${ this.props.finalPrice } <ETHPriceConverter price={this.props.finalPrice} /></span>
             </div>
           </div>
         </div>
