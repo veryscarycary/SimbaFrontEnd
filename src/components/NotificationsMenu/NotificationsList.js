8e8811783e56c640957afc9d01a87ae3d997cb71
Jeremy Hoffing <jcohenhoffing@gmail.com>
2018-01-07 16:46:06 -0800

    Add other notification states to dropdown

--- src/components/NotificationsMenu/NotificationsList.js
+++ src/components/NotificationsMenu/NotificationsList.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 const NOTIFICATION_TYPE_PURCHASE = 'purchase'
+const NOTIFICATION_TYPE_SHIP = 'ship_item'
+const NOTIFICATION_TYPE_CONFIRM = 'confirm_purchase'
+const NOTIFICATION_TYPE_CANCEL_PURCHASE = 'cancel_purchase'
+const NOTIFICATION_TYPE_CANCEL_SALE = 'cancel_sales'
 
 class NotificationsList extends Component {
   renderNotification(notification) {
@@ -15,6 +19,46 @@ class NotificationsList extends Component {
             className="dropdown-item"
           >
             {notification.actor.fullname} has bought something from you.
+          </Link>
+        )
+      case NOTIFICATION_TYPE_SHIP:
+        return (
+          <Link
+            key={notification.id}
+            to={`/purchases/${notification.target.purchase.id}/confirm`}
+            className="dropdown-item"
+          >
+            {notification.actor.fullname} has shipped your item.
+          </Link>
+        )
+      case NOTIFICATION_TYPE_CANCEL_PURCHASE:
+        return (
+          <Link
+            key={notification.id}
+            to={`/purchases/${notification.target.purchase.id}/cancel`}
+            className="dropdown-item"
+          >
+            {notification.actor.fullname} has canceled their order.
+          </Link>
+        )
+      case NOTIFICATION_TYPE_CANCEL_SALE:
+        return (
+          <Link
+            key={notification.id}
+            to={`/purchases/${notification.target.purchase.id}/cancel`}
+            className="dropdown-item"
+          >
+            {notification.actor.fullname} has canceled your order.
+          </Link>
+        )
+      case NOTIFICATION_TYPE_CONFIRM:
+        return (
+          <Link
+            key={notification.id}
+            to={`/purchases/${notification.target.purchase.id}/confirm`}
+            className="dropdown-item"
+          >
+            {notification.actor.fullname} has received your item.
           </Link>
         )
 
